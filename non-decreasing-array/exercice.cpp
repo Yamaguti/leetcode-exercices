609c0c5bb9dd31b8c63b724548a5bcca86b84af1
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2018-12-10 23:44:41 -0200

    Adding brute force solution

--- non-decreasing-array/exercice.cpp
+++ non-decreasing-array/exercice.cpp
@@ -6,8 +6,37 @@ using namespace std;
 
 class Solution {
 public:
-    bool checkPossibility(vector<int>& nums) {
+    bool isNonDecreasing(vector<int> v) {
+        for (int i = 0; i < v.size()-1; i++) {
+            if (v[i] > v[i+1]) {
+                return false;
+            }
+        }
+
         return true;
+    }
+
+    bool checkPossibility(vector<int>& nums) {
+        if (isNonDecreasing(nums)) {
+            return true;
+        }
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            int aux = nums[i];
+            nums[i] = nums[i+1];
+            if (isNonDecreasing(nums)) {
+                return true;
+            } else {
+                nums[i] = aux;
+            }
+        }
+
+        nums[nums.size()-1] = nums[nums.size()-2];
+        if (isNonDecreasing(nums)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 

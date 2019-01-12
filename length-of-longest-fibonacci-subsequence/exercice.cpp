379632ae241fe2d9735e0034b3df0ad51b512c7a
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2019-01-12 00:51:24 -0200

    Adding code scafolding

---
+++ length-of-longest-fibonacci-subsequence/exercice.cpp
@@ -0,0 +1,21 @@
+
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& A) {
+        return 0;
+    }
+};
+
+int main() {
+    Solution s = Solution();
+    vector<int> v = {1, 2, 3};
+
+    cout << "Answer: ";
+    cout << s.lenLongestFibSubseq(v) << endl;
+
+    return 0;
+}
